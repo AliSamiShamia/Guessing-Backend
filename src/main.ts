19c594f77087d227851aaddb58fe1466d7a4f507
Ali Shamia <shamiaali7@gmail.com>
2024-03-22 15:30:59 +0400

    Vercel issue

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
 
-  await app.listen(parseInt(process.env.PORT));
+  await app.listen(parseInt(process.env.PORT),()=>{
+    console.log("Connected to ")
+  });
 }
 bootstrap();
